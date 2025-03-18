351189fd3735ab5b463e7f793f192fd7838e2da4
Kushal Pangeni <64060858+KushalPangeni@users.noreply.github.com>
2025-03-18 22:34:57 +0545

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "11757155e159e5229a718583dc582960
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "349af01e04b8644902ba4117baa777dd",
+"flutter_bootstrap.js": "a46d068b4f7f48755be543eed043308a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
